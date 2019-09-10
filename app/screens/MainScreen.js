0a58d8d8690e0b31daca5d757005d354d460675c
ianchen <chenxian352@gmail.com>
2019-09-11 11:12:55 +1200

    re organized code to renderer functions.

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -19,7 +19,7 @@ class HomeScreen extends Component {
   }
 
   componentDidMount() {
-    this.props.getPokemonList()
+    this.props.getPokemonList() // bound action
   }
 
   renderListItem = (pokemon, i) => {
@@ -56,6 +56,7 @@ class HomeScreen extends Component {
         return styles.backgroundColorGreen
     }
   }
+
   renderTypes = stagePokemon => {
     if (!stagePokemon.specs.types) {
       return null
@@ -83,72 +84,88 @@ class HomeScreen extends Component {
     this.setState({ pokemonListExpanded: !this.state.pokemonListExpanded })
   }
 
+  renderStage = stagePokemon => (
+    <View style={ styles.secSlider }>
+      <TouchableOpacity style={ styles.secSliderArrowButton } onPress={ this.props.prevPokemon }>
+        <View style={ [styles.makeTriangle('left', '20', 'white')] } />
+      </TouchableOpacity>
+      <View style={ [styles.secSliderStage, { height: this.props.stageHeight }] } onLayout={ event => this.props.setStageHeight(event.nativeEvent.layout.width) }>
+        <Image style={ [styles.secSliderImage, { width: '120%', height: '120%' }] } uri={ stagePokemon.specs.sprites.front_default }/>
+      </View>
+      <TouchableOpacity style={ styles.secSliderArrowButton } onPress={ this.props.nextPokemon }>
+        <View style={ [styles.makeTriangle('right', '20', 'white')] } />
+      </TouchableOpacity>
+    </View>
+  )
+
+  renderNameAndBasis = stagePokemon => (
+    <View style={ styles.secSpecs }>
+      <View style={ [styles.flexRow, { alignItems: 'center' }] }>
+        <Text style={ styles.secSpecsName }>{ stagePokemon.name }</Text>
+        <TouchableOpacity onPress={ event => this.props.toggleFavorite(stagePokemon.name) }>
+          <AntDesign color="white" style={ { marginLeft: 6, fontSize: 18 } } name={ stagePokemon.favorite ? 'star' : 'staro' }/>
+        </TouchableOpacity>
+      </View>
+      <View style={ [styles.flexRow, { paddingTop: 2, paddingBottom: 10 }] }>
+        { this.renderTypes(stagePokemon) }
+      </View>
+    </View>
+  )
+
+  renderList = () => (
+    <Animated.View style={ [styles.secPokeList, { bottom: this.state.pokemonListAnimatedBottom }] }>
+      <TouchableWithoutFeedback onPress={ this.togglePokemonList } >
+        <View style={ { paddingTop: 10, paddingBottom: 20 } } >
+          <View style={ styles.secPokeListTrigger } />
+        </View>
+      </TouchableWithoutFeedback>
+      <ScrollView>
+        { this.renderPokemonList() }
+      </ScrollView>
+    </Animated.View>
+  )
+
+  renderSpecs = stagePokemon => (
+    <ScrollView style={ [styles.secSpecs] }>
+      <View style={ [styles.flexRow, styles.justifySpaceBetween] }>
+        <View style={ styles.secSpecsAttributeSquare }>
+          <Text>Base Exp.</Text>
+          <Text style={ styles.fontSizeLarge }>{ stagePokemon.specs.base_experience }</Text>
+        </View>
+        <View style={ styles.secSpecsAttributeSquare }>
+          <Text>Weight</Text>
+          <Text style={ styles.fontSizeLarge }>{ stagePokemon.specs.weight }</Text>
+        </View>
+        <View style={ styles.secSpecsAttributeSquare }>
+          <Text>Height</Text>
+          <Text style={ styles.fontSizeLarge }>{ stagePokemon.specs.height }</Text>
+        </View>
+      </View>
+      <View style={ [styles.flexRow, styles.secAbility] }>
+        <Text style={ [styles.fontWeightBold, styles.colorWhite, { marginRight: 10 }] }>Ability</Text>
+        { stagePokemon.specs.abilities ?
+          stagePokemon.specs.abilities.map((item, index) => {
+            return <Text key={ 'ability' + index } style={ [styles.colorWhite, { marginRight: 5, textTransform: 'capitalize' }] }>{ item.ability.name }</Text>
+          }) : null }
+      </View>
+      <View style={ styles.secStats }>
+        <Text style={ [styles.fontWeightBold, styles.colorWhite, { marginRight: 10 }] }>Stats</Text>
+        { stagePokemon.specs.stats ?
+          stagePokemon.specs.stats.map((item, i) => this.renderStat(item, i))
+          : null }
+      </View>
+    </ScrollView>
+  )
+
   render() {
     const stagePokemon = this.props.pokemonList.length > 1 ? this.props.pokemonList[this.props.currentPokemonID] : { name: 'Loading...', specs: { sprites: { front_default: CONFIGS.imagePlaceHolder }}}
     return (
       <LinearGradient colors={ ['#32799D', '#85C2E2'] } style={ styles.backdrop }>
         <SafeAreaView style={ [styles.safeView] }>
-          <View style={ styles.secSlider }>
-            <TouchableOpacity style={ styles.secSliderArrowButton } onPress={ this.props.prevPokemon }>
-              <View style={ [styles.makeTriangle('left', '20', 'white')] } />
-            </TouchableOpacity>
-            <View style={ [styles.secSliderStage, { height: this.props.stageHeight }] } onLayout={ event => this.props.setStageHeight(event.nativeEvent.layout.width) }>
-              <Image style={ [styles.secSliderImage, { width: '120%', height: '120%' }] } uri={ stagePokemon.specs.sprites.front_default }/>
-            </View>
-            <TouchableOpacity style={ styles.secSliderArrowButton } onPress={ this.props.nextPokemon }>
-              <View style={ [styles.makeTriangle('right', '20', 'white')] } />
-            </TouchableOpacity>
-          </View>
-          <View style={ styles.secSpecs }>
-            <View style={ [styles.flexRow, { alignItems: 'center' }] }>
-              <Text style={ styles.secSpecsName }>{ stagePokemon.name }</Text>
-              <TouchableOpacity onPress={ event => this.props.toggleFavorite(stagePokemon.name) }>
-                <AntDesign color="white" style={ { marginLeft: 6, fontSize: 18 } } name={ stagePokemon.favorite ? 'star' : 'staro' }/>
-              </TouchableOpacity>
-            </View>
-            <View style={ [styles.flexRow, { paddingTop: 2, paddingBottom: 10 }] }>
-              { this.renderTypes(stagePokemon) }
-            </View>
-          </View>
-          <ScrollView style={ [styles.secSpecs] }>
-            <View style={ [styles.flexRow, styles.justifySpaceBetween] }>
-              <View style={ styles.secSpecsAttributeSquare }>
-                <Text>Base Exp.</Text>
-                <Text style={ styles.fontSizeLarge }>{ stagePokemon.specs.base_experience }</Text>
-              </View>
-              <View style={ styles.secSpecsAttributeSquare }>
-                <Text>Weight</Text>
-                <Text style={ styles.fontSizeLarge }>{ stagePokemon.specs.weight }</Text>
-              </View>
-              <View style={ styles.secSpecsAttributeSquare }>
-                <Text>Height</Text>
-                <Text style={ styles.fontSizeLarge }>{ stagePokemon.specs.height }</Text>
-              </View>
-            </View>
-            <View style={ [styles.flexRow, styles.secAbility] }>
-              <Text style={ [styles.fontWeightBold, styles.colorWhite, { marginRight: 10 }] }>Ability</Text>
-              { stagePokemon.specs.abilities ?
-                stagePokemon.specs.abilities.map((item, index) => {
-                  return <Text key={ 'ability' + index } style={ [styles.colorWhite, { marginRight: 5, textTransform: 'capitalize' }] }>{ item.ability.name }</Text>
-                }) : null }
-            </View>
-            <View style={ styles.secStats }>
-              <Text style={ [styles.fontWeightBold, styles.colorWhite, { marginRight: 10 }] }>Stats</Text>
-              { stagePokemon.specs.stats ?
-                stagePokemon.specs.stats.map((item, i) => this.renderStat(item, i))
-                : null }
-            </View>
-          </ScrollView>
-          <Animated.View style={ [styles.secPokeList, { bottom: this.state.pokemonListAnimatedBottom }] }>
-            <TouchableWithoutFeedback onPress={ this.togglePokemonList } >
-              <View style={ { paddingTop: 10, paddingBottom: 20 } } >
-                <View style={ styles.secPokeListTrigger } />
-              </View>
-            </TouchableWithoutFeedback>
-            <ScrollView>
-              { this.renderPokemonList() }
-            </ScrollView>
-          </Animated.View>
+          { this.renderStage(stagePokemon) }
+          { this.renderNameAndBasis(stagePokemon) }
+          { this.renderSpecs(stagePokemon) }
+          { this.renderList(stagePokemon) }
         </SafeAreaView>
       </LinearGradient>
     )
